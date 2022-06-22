9b553126e76a1e319774911db06f032d91d6a6d4
Hudaib Mahmood <99996980+iamhudaib@users.noreply.github.com>
2022-06-22 21:29:57 +0530

    Update Sales_insight_query.sql

--- Sales_insight_query.sql
+++ Sales_insight_query.sql
@@ -9,6 +9,9 @@ Select sum(sales_amount) From sales.transactions where order_date like '2020%' a
 
 Select * From sales.transactions INNER JOIN sales.date ON sales.transactions.order_date = sales.date.date;
 
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON 
+transactions.order_date=date.date where date.year=2020 and transactions.currency="INR\r" or transactions.currency="USD\r";
+
 SELECT SUM(sales.transactions.sales_amount) FROM sales.transactions INNER JOIN sales.date ON 
 sales.transactions.order_date= sales.date.date where sales.date.year=2020 and sales.date.month_name="January" and (sales.transactions.currency="INR\r");
 
